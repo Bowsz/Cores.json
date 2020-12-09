bb097b908edc39a96dfd688201b2b7dfed54a3a2
Bowsz <74607029+Bowsz@users.noreply.github.com>
2020-12-09 12:04:32 -0300

    Add files via upload

---
+++ Comousar.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js")
+//Você irá puxar o nome da cor na qual você desejar juntamente com o arquivo que ele está.
+const { salmão } = require("../../cores.json")
+
+exports.run = async (bot, message, args) => {
+
+    const bow = new Discord.RichEmbed()
+        .setAuthor("Opa.")
+        .setColor(salmão)
+        .setDescription("Olá.")
+        .setTimestamp()
+        .setFooter("hm")
+    message.channel.send(bow)
+
+}
+
+
+exports.help = {
+    name: "hm",
+    descricao: "'-'",
+    aliases: ["teste", "hm", "asasda"],
+    icon: null
+}
